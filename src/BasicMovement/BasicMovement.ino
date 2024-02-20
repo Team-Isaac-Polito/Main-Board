ce0dff3c5d83c5a7448a5781e3e0f6a65f4794a7
Simone Torino <simone.torino.12@gmail.com>
2024-02-20 19:47:43 +0100

    I2C only test - working

--- src/BasicMovement/BasicMovement.ino
+++ src/BasicMovement/BasicMovement.ino
@@ -6,30 +6,37 @@
 CAN_FRAME CanMsg;
 
 float leftSpeed, rightSpeed;
+int l = 20, r = 10;
 
 // print CAN frame
-void printFrame(CAN_FRAME *message) {
-   Serial.print(message->id, HEX);
-   if (message->extended) {
-      Serial.print(" X ");
-   } else {
-      Serial.print(" S ");
-   }
-   Serial.print(message->length, DEC);
-   for (int i = 0; i < message->length; i++) {
-      Serial.print(message->data.byte[i], HEX);
-      Serial.print(" ");
-   }
-   Serial.println();
+void printFrame(CAN_FRAME *message)
+{
+  Serial.print(message->id, HEX);
+  if (message->extended)
+  {
+    Serial.print(" X ");
+  }
+  else
+  {
+    Serial.print(" S ");
+  }
+  Serial.print(message->length, DEC);
+  for (int i = 0; i < message->length; i++)
+  {
+    Serial.print(message->data.byte[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   while (!Serial)
     ;
   Serial.println("Initializing...");
 
-  //Initializing I2C
+  // Initializing I2C
   Wire.begin(17, 18);
 
   // activate GND_sw
@@ -39,25 +46,29 @@ void setup() {
   // configure CAN
   CAN0.setCANPins((gpio_num_t)36, (gpio_num_t)35);
   CAN0.begin(125000);
-  CAN0.watchFor();  // use this to filter for specific IDs
+  CAN0.watchFor(); // use this to filter for specific IDs
 
   Serial.println("Ready!");
 }
 
-int scale(int m) {
-  return (m+55)*(254)/110 - 127; // -127,127
-  //return (m+55)*(510)/110 - 255; // -255,255
-  //return (m+55)*(2048)/110 - 1024; // -1024,1024
+int scale(int m)
+{
+  return (m + 55) * (254) / 110 - 127; // -127,127
+  // return (m+55)*(510)/110 - 255; // -255,255
+  // return (m+55)*(2048)/110 - 1024; // -1024,1024
 }
 
-void loop() {
+void loop()
+{
   // CAN read message
-  if (CAN0.read(CanMsg)) {
+  /* if (CAN0.read(CanMsg))
+  {
     Serial.println("ESP32 received message");
     printFrame(&CanMsg);
 
-    if ((CanMsg.id & 0x00FF0000)>>16 == 0x21) {
-       Serial.println("MOVEMENT MESSAGE");
+    if ((CanMsg.id & 0x00FF0000) >> 16 == 0x21)
+    {
+      Serial.println("MOVEMENT MESSAGE");
 
       // take the first four bytes from the array and and put them in a float
       memcpy(&leftSpeed, &(CanMsg.data), 4);
@@ -65,15 +76,15 @@ void loop() {
       // motorTrLeft.setSpeed(leftSpeed);
       // motorTrRight.setSpeed(rightSpeed);
 
-      //I2C Send data to motors
+      // I2C Send data to motors
       int left = scale((int)leftSpeed);
       int right = scale((int)rightSpeed);
       Wire.beginTransmission(SLAVE_ADDR);
       Wire.write(left);
       Wire.write(right);
-      byte error = Wire.endTransmission();
-      Serial.print("Result: ");
-      Serial.println(error);
+      byte ecode = Wire.endTransmission();
+      Serial.print("I2C exit code: ");
+      Serial.println(ecode);
       Serial.println("Receive data");
 
       Serial.print("TRACTION DATA :\tleft: \t");
@@ -83,5 +94,21 @@ void loop() {
     }
   }
 
-  delay(5);
+ */
+  
+  // Test I2C only
+  l++;
+  r++;
+  Wire.beginTransmission(SLAVE_ADDR);
+  Serial.print("Sending values: ");
+  Serial.print(l);
+  Serial.print(" ");
+  Serial.println(r);
+  Wire.write(l);
+  Wire.write(r);
+  byte ecode = Wire.endTransmission();
+  Serial.print("I2C exit code: ");
+  Serial.println(ecode);
+
+  delay(500);
 }
