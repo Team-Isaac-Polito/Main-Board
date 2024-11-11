56d2ef16ea96f5e3e62d2ee9c50f961952bb8013
Marcello Ceveriati <marcello.ceveriati@hotmail.com>
2024-11-11 17:26:09 +0100

    Update MainI2C.h

--- main_I2C/MainI2C.h
+++ main_I2C/MainI2C.h
@@ -1,1 +1,18 @@
+#ifndef MAIN_I2C_H
+#define MAIN_I2C_H
 
+#include <Wire.h>
+
+class MainI2C {
+  public:
+    MainI2C(TwoWire& wire, int slaveAddress);
+    void begin(int sdaPin, int sclPin, uint32_t frequency = 100000);  // Initialize I2C with custom pins
+    void sendMotorSpeeds(float leftSpeed, float rightSpeed);          // Send motor speeds to the slave
+    void requestTelemetry(float* leftSpeedTelemetry, float* rightSpeedTelemetry);  // Request telemetry data
+
+  private:
+    TwoWire& wireInstance;
+    int slaveAddress;
+};
+
+#endif
